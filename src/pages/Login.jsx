713a7630d86f2a1227796781503efe039ed5a552
Guille <lunaalvarezguillermo96@gmail.com>
2025-07-20 11:06:32 +0200

    Modificados aspectos visuales para marcar al usuario que es lo que esta sucediendo al iniciar sesion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,36 +4,45 @@ import { useNavigate } from "react-router-dom";
 import {TextField, Button, Typography, Container, Box, Paper } from '@mui/material';
 import { API_BASE_URL } from '../config';
 import PageWrapper from '../components/PageWrapper';
+
+
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false); 
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
 
 const handleLogin = async () => {
-    try {
-      const response = await fetch(`${API_BASE_URL}/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
+  setLoading(true);
+  setError("");
 
-      if (!response.ok) {
-        throw new Error("Credenciales incorrectas");
-      }
+  try {
+    const response = await fetch(`${API_BASE_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-      const data = await response.json();
-      localStorage.setItem("token", data.token);
-
-      // Redirigimos al perfil
-      navigate("/profile");
-    } catch (error) {
-      console.error("Error al iniciar sesión:", error.message);
-      alert("Error al iniciar sesión");
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Credenciales incorrectas");
     }
-  };
+
+    const data = await response.json();
+    localStorage.setItem("token", data.token);
+    navigate("/profile");
+
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error.message);
+    setError(error.message); 
+  } finally {
+    setLoading(false);
+  }
+};
 
 return (
     <PageWrapper>
@@ -66,14 +75,21 @@ return (
             onChange={e => setPassword(e.target.value)}
           />
           <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            onClick={handleLogin}
-            sx={{ mt: 2 }}
-          >
-            Iniciar Sesión
+           variant="contained"
+           color="primary"
+           fullWidth
+           onClick={handleLogin}
+           sx={{ mt: 2 }}
+           disabled={loading} // bloquea si está cargando
+>
+              {loading ? "Cargando..." : "Iniciar Sesión"}
           </Button>
+
+{error && (
+  <Typography variant="body2" color="error" mt={2}>
+    {error}
+  </Typography>
+)}
         </Paper>
       </Box>
     </PageWrapper>
